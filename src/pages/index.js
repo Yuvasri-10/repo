2be99f0f3aafd3e72a9481d0ebef130a1170f571
Yuvasri-10 <71762307062@cit.edu.in>
2025-06-12 21:16:57 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ const Home = () => {
       <header>
         <h1>AceIt</h1>
         <p>Your Monthly Productivity Tracker</p>
-        <nav style={{ display: 'flex', gap: '10px', text-align: center }}>
+        <nav style={{ display: 'flex', gap: '10px', textAlign: center }}>
           <Link href="/">Home</Link>
           <Link href="/services">Planner</Link>
           <Link href="/tracker">Tracker</Link>
